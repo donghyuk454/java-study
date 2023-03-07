05c2d97d1f0273552e0c343ba07134f67e048da2
dongdong <donghyuk454@gmail.com>
2023-03-08 00:11:57 +0900

    feat: chap7 학습에 대한 내용 추가

---
+++ src/chap7/Chap7Application.java
@@ -0,0 +1,10 @@
+package chap7;
+
+import chap6.ClassVariables;
+
+public class Chap7Application {
+    public static void main(String[] args) {
+//        같은 패키지 아래에 있지 않아서 오류 발생!
+//        ClassVariables.checkNumStatic = 3;
+    }
+}
