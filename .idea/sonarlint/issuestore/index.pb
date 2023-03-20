c8074db162fabb163b5018eaf5627a7d5cd97ee5
dongdong <donghyuk454@gmail.com>
2023-03-21 00:56:10 +0900

    Feat: chap11 LinkedList 실습 및 중복 코드 리팩토링

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,15 @@
 
 =
 workspace.iml,9\4\94fa2f1797d8932cd04cb8d44c0413a8d268a614
+b
+2src/main/java/com/dong/chap9/Chap9Application.java,f\6\f664cb8e36ea78311664eccf9f6876aca2405009
+b
+2src/main/java/com/dong/chap7/Chap7Application.java,2\7\279f9f1f451c77a161465ef33eab2ac6cd0cd896
+`
+0src/main/java/com/dong/chap6/ClassVariables.java,6\1\61a1a7a71568de7fab822509df770ba6332a5a80
+b
+2src/main/java/com/dong/chap6/Chap6Application.java,2\a\2ac5158226bf7e4daac843d0dd4138b00d4ea7c6
+^
+.src/main/java/com/dong/chap9/ObjectSample.java,0\0\00e16ed478d45fa0c3eb54c8b90ce11e1f1e7723
+]
+-src/main/java/com/dong/chap9/CloneSample.java,0\7\07fb5f0ab19bd638f8a4fb01a078e5af49f62b97
--- src/main/java/com/dong/chap11/Chap11Application.java
+++ src/main/java/com/dong/chap11/Chap11Application.java
@@ -1,37 +1,56 @@
 package main.java.com.dong.chap11;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Chap11Application {
+    private static int count = 100000;
+
     public static void main(String[] args) {
 
         // 그냥 생성 시 10개의 객체만 받을 정도로만 생성함
         // 미리 capacity 를 입력해주면 성능을 향상시킬 수 있음
-        int count = 100000;
-        long startTime = System.currentTimeMillis();
-        List list1 = new ArrayList<SampleData>();
 
-        for (int i = 0; i < count; i++) {
-            list1.add(new SampleData(1, "hi"));
-        }
+        List arrayList = new ArrayList<SampleData>();
+        List fixedSizeArrayList = new ArrayList<SampleData>(count + 10000);
+        List linkedList = new LinkedList<SampleData>();
 
-        long endTime = System.currentTimeMillis();
-        long time = endTime - startTime;
-        System.out.println("time = " + time); // time = 14
-
-        startTime = System.currentTimeMillis();
-        List list2 = new ArrayList<SampleData>(count + 10000);
-
-        for (int i = 0; i < count; i++) {
-            list1.add(new SampleData(1, "hi"));
-        }
-
-        endTime = System.currentTimeMillis();
-        time = endTime - startTime;
-        System.out.println("time = " + time); // time = 5
+        addItemInOrder(arrayList);         // time = 18 (첫 실행 시 오래 걸림..)
+        addItemInOrder(fixedSizeArrayList);         // time = 4
 
         // 하지만 count 를 1000000 으로 할 경우 전자가 더 성능이 좋음.
         // 이유를 모르겠다.
+
+        // linked list 가 순차적인 생성에서 더 오래 걸림
+        addItemInOrder(arrayList);         // time = 5
+        addItemInOrder(linkedList);    // time = 19
+
+        // linked list 가 중간에 추가할 땐 더 짧게 걸림
+        addItemInMiddle(arrayList);         // time = 4419
+        addItemInMiddle(linkedList);    // time = 105
+
+    }
+
+    private static void addItemInOrder(List list) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            list.add(new SampleData(1, "hi"));
+        }
+        long endTime = System.currentTimeMillis();
+        printTime(startTime, endTime);
+    }
+
+    private static void addItemInMiddle(List list) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            list.add(500, new SampleData(1, "hi"));
+        }
+        long endTime = System.currentTimeMillis();
+        printTime(startTime, endTime);
+    }
+
+    private static void printTime(long start, long end) {
+        System.out.println("time = " + (end - start));
     }
 }
