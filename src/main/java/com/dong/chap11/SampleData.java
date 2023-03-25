90fb6338428cffa142597aa2e16c6015be041d66
dongdong <donghyuk454@gmail.com>
2023-03-26 02:16:53 +0900

    Refactor: getter, setter 추가

--- src/main/java/com/dong/chap11/SampleData.java
+++ src/main/java/com/dong/chap11/SampleData.java
@@ -1,11 +1,27 @@
 package main.java.com.dong.chap11;
 
 public class SampleData {
-    int a;
-    String b;
+    private int a;
+    private String b;
 
     public SampleData(int a, String b) {
         this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(String b) {
         this.b = b;
     }
 }
