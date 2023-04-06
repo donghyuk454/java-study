9f62ddc2911daede21d6e3baf9f074f48ee39260
dongdong <donghyuk454@gmail.com>
2023-04-07 01:19:09 +0900

    Feat: aop 구현을 위한 domain 개발

---
+++ src/main/java/com/dong/chap12/aop/domain/MyInfo.java
@@ -0,0 +1,37 @@
+package main.java.com.dong.chap12.aop.domain;
+
+public class MyInfo {
+    private String name;
+    private int age;
+    private String[] hobbies;
+
+    public MyInfo(String name, int age, String[] hobbies) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+}
