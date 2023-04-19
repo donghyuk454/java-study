48c6c1b06f6f3509f65bf217ad66624c8db0d2cd
dongdong <donghyuk454@gmail.com>
2023-04-20 00:49:32 +0900

    Feat: join 기능 개발

--- src/main/java/com/dong/chap13/AdvancedThread.java
+++ src/main/java/com/dong/chap13/AdvancedThread.java
@@ -2,8 +2,11 @@ package main.java.com.dong.chap13;
 
 public class AdvancedThread implements Runnable {
 
+    private static final int MAX_COUNT = 10;
+
     private boolean stopped = false;
     private boolean suspended = false;
+    private int count = 0;
 
     private final Thread thread;
 
@@ -13,9 +16,10 @@ public class AdvancedThread implements Runnable {
 
     @Override
     public void run() {
-        while (!stopped) {
+        while (!stopped && getCount() < MAX_COUNT) {
             if (!suspended) {
                 try {
+                    System.out.println(this.thread.getName() + " sleep~ Zzz | count : " + getCount());
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     System.out.println("InterruptedException 발생 !");
@@ -24,7 +28,12 @@ public class AdvancedThread implements Runnable {
                 // 대기로 이동
                 Thread.yield();
             }
+            setCount( getCount() + 1 );
         }
+    }
+
+    public void interrupt() {
+        this.thread.interrupt();
     }
 
     public void stop() {
@@ -37,11 +46,27 @@ public class AdvancedThread implements Runnable {
         thread.interrupt(); // sleep 상태일 경우 InterruptedException 을 발생시키기 위해
     }
 
+    public void join() throws InterruptedException {
+        this.thread.join();
+    }
+
+    public void join(long millis) throws InterruptedException {
+        this.thread.join(millis);
+    }
+
     public void resume() {
         this.suspended = false;
     }
 
     public void start() {
         thread.start();
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 }
