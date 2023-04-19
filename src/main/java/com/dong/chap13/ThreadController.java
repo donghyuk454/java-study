aed0e18ce472a7b5b2c148d2783283c5b494658a
dongdong <donghyuk454@gmail.com>
2023-04-20 00:49:48 +0900

    Feat: join 실습 진행

--- src/main/java/com/dong/chap13/ThreadController.java
+++ src/main/java/com/dong/chap13/ThreadController.java
@@ -8,7 +8,7 @@ public class ThreadController {
     }
 
     private void control () throws InterruptedException {
-        NewThread newThread = new NewThread();
+        NewThread newThread = new NewThread("-");
         newThread.start();
         Thread.sleep(1);
 
@@ -25,14 +25,46 @@ public class ThreadController {
         Thread.sleep(500);
         newSleepThread.interrupt();
 
+        /* sleep 인 경우엔 join 이 정상 작동하지 않는다... */
+        AdvancedThread advancedThread1 = new AdvancedThread("thread 1");
+        AdvancedThread advancedThread2 = new AdvancedThread("thread 2");
+        advancedThread1.start();
+        advancedThread2.start();
+
+        Thread.sleep(2000);
+        advancedThread1.interrupt(); // main 쓰레드가 advancedThread1 을 기다림
+        advancedThread1.join(1500);
+        advancedThread1.interrupt();
+        Thread.sleep(2000);
+        advancedThread2.join(); // main 쓰레드가 advancedThread2 을 기다림
+
+        // 따라서 advancedThread2 가 끝날때까지 실행되지 않음
+        NewThread newThread1 = new NewThread("1");
+        NewThread newThread2 = new NewThread("2");
+        newThread1.start();
+        newThread2.start();
+
+        Thread.sleep(5);
+        newThread1.join(10); // main 쓰레드가 newThread1 을 기다림
+        newThread2.join(10); // main 쓰레드가 newThread2 를 기다림
+        Thread.sleep(100);
+        System.out.println("interrupt!!");
+        newThread1.interrupt();
+        newThread2.interrupt();
+        Thread.sleep(100);
+        System.out.println("End !");
     }
 
     static class NewThread extends Thread {
+        public NewThread ( String name ) {
+            super(name);
+        }
+
         @Override
         public void run() {
             System.out.println("thread start");
             while (!interrupted()) { // interrupt 되기 전까지 안에 있는 코드가 실행됨
-                System.out.print("-");
+                System.out.print(this.getName());
             }
             System.out.println("New Thread is interrupted !");
         }
