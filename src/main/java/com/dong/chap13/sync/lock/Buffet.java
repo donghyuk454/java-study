200aa75f8b13ecdedcf683610b1ea2d10e52bb05
dongdong <donghyuk454@gmail.com>
2023-04-25 22:42:03 +0900

    FEAT: 싱글톤 패턴 동기화 4가지 방법 적용

--- src/main/java/com/dong/chap13/sync/lock/Buffet.java
+++ src/main/java/com/dong/chap13/sync/lock/Buffet.java
@@ -11,12 +11,38 @@ public class Buffet {
 
     private Buffet() { }
 
-    public static Buffet getInstance() {
-        if (buffet == null) {
-            buffet = new Buffet();
-        }
-        return buffet;
+
+    // 싱글톤 패턴 동기화 ( static class 를 사용한 Lazy Initialization )
+    private static class BuffetHolder {
+        private static final Buffet INSTANCE = new Buffet();
     }
+
+    public static Buffet getInstance() {
+        return BuffetHolder.INSTANCE;
+    }
+
+//    //싱글톤 패턴 동기화 ( Eager Initialization )
+//    private static Buffet buffet = new Buffet();
+
+//     //싱글톤 패턴 동기화 ( double checked lock )
+//    public static Buffet getInstance() {
+//        if (buffet == null) {
+//            synchronized (Buffet.class) {
+//                if (buffet == null) {
+//                    buffet = new Buffet();
+//                }
+//            }
+//        }
+//        return buffet;
+//    }
+
+//    // 싱글톤 패턴 동기화 ( synchronized )
+//    public synchronized static Buffet getInstance() {
+//        if (buffet == null) {
+//            buffet = new Buffet();
+//        }
+//        return buffet;
+//    }
 
     public void addFoodCount(int number) {
         lock.lock();
