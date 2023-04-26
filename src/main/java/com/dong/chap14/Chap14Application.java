c307a99c11388bf7333ae08a217237367f6b0af6
dongdong <donghyuk454@gmail.com>
2023-04-27 00:41:27 +0900

    FEAT: Function 합성, Predicate 결합 실습

--- src/main/java/com/dong/chap14/Chap14Application.java
+++ src/main/java/com/dong/chap14/Chap14Application.java
@@ -1,5 +1,8 @@
 package main.java.com.dong.chap14;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class Chap14Application {
     public static void main(String[] args) {
 
@@ -65,5 +68,28 @@ public class Chap14Application {
         // BinaryOperator -> parameter : T, T, return : T
         String binaryOperatorResult = SampleMethods.doBinaryOperator((v1, v2) -> v1 + v2, "내 이름은 ", "이동혁이야");
         System.out.println("binaryOperatorResult = " + binaryOperatorResult);
+
+
+        // Function 합성
+        // andThen 과 compose 의 차이 : 합성의 순서
+        Function<Integer, String> function1 = Object::toString;
+        Function<String, Integer> function2 = v -> Integer.parseInt(v + "0");
+
+        Function<Integer, Integer> multiple1 = function1.andThen(function2);
+        Function<String, String> multiple2 = function1.compose(function2);
+
+        System.out.println("value = " + multiple1.apply(17));
+        System.out.println("value = " + multiple2.apply("10"));
+
+        // Predicate 결합
+        Predicate<Integer> predicate1 = v -> v > 100;
+        Predicate<Integer> predicate2 = v -> v < 200;
+        Predicate<Integer> predicate3 = v -> v > 300;
+
+        // 100 초과 200 미만 또는 300 초과 확인
+        int number = 150;
+        System.out.println("number = " + number + " -> result : " + predicate1.and(predicate2).or(predicate3).test(number));
+        // 100 이하 또는 200 초과 그리고 300 이하 확인
+        System.out.println("number = " + number + " -> result : " + predicate1.and(predicate2).or(predicate3).negate().test(number));
     }
 }
