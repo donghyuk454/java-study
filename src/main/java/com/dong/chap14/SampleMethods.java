1cd3505cf9294fff50250316a57e0bc029033335
dongdong <donghyuk454@gmail.com>
2023-04-26 22:11:54 +0900

    FEAT: 다양한 함수형 인터페이스 기능을 사용하기 위한 static 한 매서드로 정의

---
+++ src/main/java/com/dong/chap14/SampleMethods.java
@@ -0,0 +1,44 @@
+package main.java.com.dong.chap14;
+
+import java.util.function.*;
+
+public class SampleMethods {
+
+    private SampleMethods() {}
+
+    public static String doSupplier(Supplier<String> supplier) {
+        return supplier.get();
+    }
+
+    public static void doConsumer(Consumer<String> consumer, String value) {
+        consumer.accept(value);
+    }
+
+    public static String doFunction(Function<Integer, String> function, Integer value) {
+        return function.apply(value);
+    }
+
+    public static boolean doPredicate(Predicate<String> predicate, String value) {
+        return predicate.test(value);
+    }
+
+    public static void doBiConsumer(BiConsumer<String, Integer> biConsumer, String value1, Integer value2) {
+        biConsumer.accept(value1, value2);
+    }
+
+    public static <T, U> boolean doBiPredicate(BiPredicate<T, U> biPredicate, T t, U u) {
+        return biPredicate.test(t, u);
+    }
+
+    public static <T, U> boolean doBiFunction(BiFunction<T,U,Boolean> biFunction, T t, U u) {
+        return biFunction.apply(t,u);
+    }
+
+    public static String doUnaryOperator(UnaryOperator<String> operator, String value) {
+        return operator.apply(value);
+    }
+
+    public static <T> String doBinaryOperator(BinaryOperator<T> operator, T t1, T t2) {
+        return operator.apply(t1, t2).toString();
+    }
+}
