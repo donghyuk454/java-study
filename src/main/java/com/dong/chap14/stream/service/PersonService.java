c3ca9eb51a8afc30822c171e5b035e86a14ae625
dongdong <donghyuk454@gmail.com>
2023-04-29 22:55:25 +0900

    FEAT: 주요 비즈니스 로직 추가 (id 로 Person 조회, 직업 변경, Name 으로 id 조회, 동일 직업을 가진 Person 이름 조회)

--- src/main/java/com/dong/chap14/stream/service/PersonService.java
+++ src/main/java/com/dong/chap14/stream/service/PersonService.java
@@ -1,15 +1,59 @@
 package main.java.com.dong.chap14.stream.service;
 
+import main.java.com.dong.chap14.stream.entity.Person;
+import main.java.com.dong.chap14.stream.enums.Job;
+import main.java.com.dong.chap14.stream.repository.PersonRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PersonService {
 
-    private PersonService() {}
+    private static PersonService personService;
 
-    private static class PersonServiceHolder {
-        private static final PersonService INSTANCE = new PersonService();
+    private final PersonRepository personRepository;
+
+    private PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
     }
 
-    public static PersonService getInstance() {
-        return PersonServiceHolder.INSTANCE;
+    public static PersonService getInstance(PersonRepository personRepository) {
+        if (personService == null) {
+            personService = new PersonService(personRepository);
+        }
+        return personService;
     }
 
+    public Person findPersonById (Long id) {
+        return personRepository.findById(id)
+                .orElse(null);
+    }
+
+    public Person changeJob (Long id, Job job) {
+        Person person = personRepository.findById(id)
+                .orElse(null);
+
+        if (person == null)
+            return null;
+
+        person.setJob(job);
+
+        return personRepository.save(person);
+    }
+
+    public List<String> getPersonNameListByJob (Job job) {
+        List<Person> personList =
+                personRepository.findByJob(job);
+
+        return personList.stream()
+                .map(Person::getName)
+                .collect(Collectors.toList());
+    }
+
+    public Long getIdByName (String name) {
+        Person person = personRepository.findByMame(name)
+                .orElse(null);
+
+        return person == null ? null : person.getId();
+    }
 }
