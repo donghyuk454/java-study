77e640b198bb809a3091850f7e6936f01d109d8c
dongdong <donghyuk454@gmail.com>
2023-05-01 17:14:53 +0900

    FEAT: findAll 과 find popular job 추가

--- src/main/java/com/dong/chap14/stream/repository/PersonRepository.java
+++ src/main/java/com/dong/chap14/stream/repository/PersonRepository.java
@@ -4,12 +4,15 @@ import main.java.com.dong.chap14.stream.enums.Job;
 import main.java.com.dong.chap14.stream.entity.Person;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface PersonRepository {
+    List<Person> findAll();
     Optional<Person> findById(long id);
     Person save(Person person);
     Optional<Person> findByMame(String name);
     List<Person> findByJob(Job job);
     Optional<Person> findMaxAgePerson();
+    Map<Job, Long> findPopularJob();
 }
