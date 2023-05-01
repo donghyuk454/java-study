452a5fa86c717e8de50eae9a1af46a0c178ebd6f
dongdong <donghyuk454@gmail.com>
2023-05-01 17:14:22 +0900

    FEAT: 가장 인기있는 직업에 대한 결과를 담을 dto 정의

---
+++ src/main/java/com/dong/chap14/stream/controller/response/PopularJobDto.java
@@ -0,0 +1,30 @@
+package main.java.com.dong.chap14.stream.controller.response;
+
+import main.java.com.dong.chap14.stream.enums.Job;
+
+public class PopularJobDto {
+
+    private final Job job;
+    private final Long count;
+
+    public PopularJobDto(Job job, Long count) {
+        this.job = job;
+        this.count = count;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "PopularJobDto{" +
+                "job=" + job +
+                ", count=" + count +
+                '}';
+    }
+}
